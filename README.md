baa7c8a01c72f993a68d383c0ffe2b315c6524c9
Yip Ka Yan <yipkayan3@gmail.com>
2023-03-29 14:29:44 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # profile
-profile page
+profile page created using React.
+
+Host using Github.
+
+
+Github url: https://github.com/kyip3/profile
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Ka Yan.Yip</h2>
+        <p>Software Engineer</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
